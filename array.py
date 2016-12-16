113d5ad7f56d18c41b0ba5dae26a7c6383ec7908
xutu <billxu89@gmail.com>
2016-12-16 11:54:01 +0100

    more

--- array.py
+++ array.py
@@ -4,6 +4,34 @@ print(family[-1])
 
 print('abcd'[2])
 print("abcd"[3])
-
+print("----------------")
 number = [1,2,3,4,5,6]
 print(number[0:4])
+print(number[-3:-1])
+print(number[-3:])
+print(number[:3])
+print(number[:])
+print(number[0:6:2]) # how much increment
+print(number[6:0:-2]) #backwards
+print(number[::-2])
+print("---------------")
+x = [9,8,7] + [1,2,3]
+print(x)
+print("---------------")
+# array functions
+numbers = [2,45,66,88,26,8,98]
+print(len(numbers))
+print(max(numbers),min(numbers))
+print(list("haha xutu"))
+numbers[1] = 68
+print(numbers)
+del numbers[5]
+print(numbers)
+del numbers[:3]
+print(numbers)
+
+
+
+
+
+
--- string.py
+++ string.py
@@ -7,4 +7,24 @@ num = str(6)
 print(a+num)
 
 num2 = 10
-print("hah" + num2)
+#print("hah" + num2)
+
+print("--------")
+a = 'xu'
+print(a*5)
+print([4]*5)
+
+print("---------")
+name = 'roastbeef'
+print('z' in name)
+print('r' in name)
+
+name = "roastbeef"
+print("z" in name)
+print("r" in name)
+print("beef" in name)
+
+print("---------")
+family = ['mom','dad','bro']
+print('sisi' in family)
+print('dad' in family)
