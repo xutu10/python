f7cee4e4557b5d266f11456401376568b103c574
xutu <billxu89@gmail.com>
2017-01-17 20:55:52 +0100

    finish but wrong

--- ai/naive_bayes/classifier.py
+++ ai/naive_bayes/classifier.py
@@ -5,6 +5,8 @@ from preprocess_documents import read_and_preprocess_documents
 import sys
 import argparse
 import pickle
+import operator
+import numpy
 """
 
   KI Exercise "Naive Bayes" 
@@ -120,22 +122,21 @@ class NaiveBayesClassifier():
 
     def train(self, features, labels):
         # FIXME: implement (Exercise 02)
-        counter = 0
+        counter = 0.0
 
         # beginning of for loop
         for k in features.keys():
-            counter += 1
+            counter += 1.0
             class_name = labels[k]
 
             # handle self.pc
             if class_name in self.pc.keys():
-                self.pc[class_name] += 1.0
+                self.pc[class_name] += 1
             else:                                          # class occurs first time
-                self.pc.update({class_name:1.0})           # add class_name as key in self.pc
+                self.pc.update({class_name:1})           # add class_name as key in self.pc
         
             # handle self.pxc
-                                                           # store subdict in features
-            dict_temp = features[k]
+            dict_temp = features[k]                         # store subdict in features
                                                         
             if class_name in self.pxc.keys():               # whether class already as key in self.pxc
                 self.pxc[class_name]['class_counter'] += 1                 # increment counter of class
@@ -143,35 +144,56 @@ class NaiveBayesClassifier():
                     if key in self.pxc[class_name].keys():   # whether word as key already in subdict of self.pxc 
                         self.pxc[class_name][key] += 1                               # increment value of word
                     else:
-                        self.pxc[class_name].update({key:1.0})           # add new word in subdict
-            else:
-                                                                         # class name occurs first time
-                                                                         # add class name as key in self.pxc
-                                                                         # and add classcounter as key in subdict of self.pxc
-                self.pxc.update({class_name:{}})
-                self.pxc[class_name].update({'class_counter':1})
-
+                        self.pxc[class_name].update({key:1})             # add new word in subdict
+            else:                                                        # class name occurs first time
+                self.pxc.update({class_name:{}})                         # add class name as key in self.pxc
+                self.pxc[class_name].update({'class_counter':1.0})       # and add classcounter as key in subdict of self.pxc
                 for key in dict_temp.keys():                            
-                    self.pxc[class_name].update({key:1.0})     # add word in subdict of self.pxc                                                 
+                    self.pxc[class_name].update({key:1})                 # add word in subdict of self.pxc                                                 
+
         # ending of for loop
 
-        # calculate probability for each class in self.pc
-        for k in self.pc.keys():
+        for k in self.pc.keys():        # calculate probability for each class in self.pc
             self.pc[k] /= counter
 
-        #calculate pro for each word in each class
-        for k in self.pxc.keys():
+        for k in self.pxc.keys():        #calculate pro for each word in each class
             counter_tmp = self.pxc[k]['class_counter']                 # store counter of class
-            del self.pxc[k]['class_counter']                            # remove the counter item from subdict
+            del self.pxc[k]['class_counter']                           # remove the counter item from subdict
     
             for key in self.pxc[k].keys():
-                self.pxc[k][key] /= counter_tmp                     # frequency divided by freqeuency of class
+                self.pxc[k][key] /= counter_tmp                        # frequency of word divided by freqeuency of class
 
-#        raise NotImplementedError()
-
+                
     def apply(self, features):
         # FIXME: implement (Exercise 03)
-        raise NotImplementedError()
+        unexpect = numpy.log10(0.000001)
+        rslt = {}
+       
+        for docu_name in features.keys():
+            comparing_temp = {'arts':0,'business':0,'dining':0,           # each docu has a temp dict to select the class with max pro
+                              'health':0,'sports':0,'technology':0,
+                              'travel':0,'world':0}
+
+            for class_name in self.pxc.keys():                               # walk through the classifier of each class
+                class_pro = 0.0
+                class_classifier = self.pxc[class_name]                      # store pxc of the class in tmp dict
+                for word_name in features[docu_name].keys():                            # walk through all features of the docu
+                    if word_name in class_classifier.keys():
+                        class_pro += numpy.log10(class_classifier[word_name])      # multiple p(x|c)
+                        del class_classifier[word_name]                      # remove this item in tmp dict
+                    else:
+                        class_pro += unexpect                                # the word not in classifier
+
+                for word_name in class_classifier.keys():                    # through all word which not in docu feature, multiple 1-p
+                    class_pro += numpy.log10(1-class_classifier[word_name])      
+
+                class_pro += numpy.log10( self.pc[class_name])      
+                comparing_temp[class_name] = class_pro
+
+            docu_class = max(comparing_temp.iteritems(), key=operator.itemgetter(1))[0]
+            rslt.update({docu_name:docu_class})
+            print docu_name, docu_class
+        return rslt
 
 
 if __name__ == "__main__":
@@ -206,17 +228,27 @@ if __name__ == "__main__":
     # for k,v in features.items():
     #     print k,'-',v,
     # print '-----------'
-    classifier = NaiveBayesClassifier()
-
+    #classifier = NaiveBayesClassifier()
+    classifier =  pickle.load(file("classifier.pickle","rb"))
+#    print classifier.pc
+#    print classifier.pxc
     #  train classifier on 'features' and 'labels' 
     # (using documents from the 'train' folder)
-   if args.train:
-       classifier.train(features, labels)
-       pickle.dump(classifier,file("classifier.pickle","wb"))
+    if args.train:
+        classifier.train(features, labels)
+        pickle.dump(classifier,file("classifier.pickle","wb"))
     # apply the classifier to documents from
     # the 'test' folder
+    result ={} 
     if args.apply:
         result = classifier.apply(features)
         
     # FIXME: measure error rate on 'test' folder (Exercise 04)
-
+    error_counter = 0.0
+    docu_counter = 0
+    for docu_name in features.keys():
+        docu_counter += 1
+        if labels[docu_name] != result[docu_name]:
+            error_counter += 1
+    print error_counter,'-', docu_counter
+    print 'error rate :', error_counter/docu_counter
